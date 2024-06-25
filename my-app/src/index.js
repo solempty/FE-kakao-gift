05b3827a3159b19f44abab90aa3ed1c826154977
Solbin Jung <jsolbin0724@gmail.com>
2024-06-25 23:53:50 +0900

    새로 만든 파일 지우고 'src'폴더를 기준으로 절대 경로 지정, '@/'로 시작하는 모든 모듈 경로를 현재 기준 경로로 지정

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//절대경로 import
-import MyComponent from '@components/MyComponent';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
