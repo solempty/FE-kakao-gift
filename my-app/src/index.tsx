500a0970fa668d35749c435714b15a787406d84e
Solbin Jung <jsolbin0724@gmail.com>
2024-06-27 02:03:58 +0900

    refactor: 나만의 기준으로 폴더 구조 정리

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Global, css } from '@emotion/react';
-import App from './App';
+import App from './components/App';
 
 import reset from 'reset-css';
 
